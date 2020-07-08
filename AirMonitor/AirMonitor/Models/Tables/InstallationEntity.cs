37fce183801d53b9574040b6890b53833ede39a3
kamilwegrzyn <48628515+kamilwegrzyn@users.noreply.github.com>
2020-07-09 00:13:31 +0200

    lab5

---
+++ AirMonitor/AirMonitor/Models/Tables/InstallationEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+using SQLite;
+
+namespace AirMonitor.Models.Tables
+{
+    public class InstallationEntity
+    {
+        //konstruktor pusty
+        public InstallationEntity()
+        {
+        }
+
+        public InstallationEntity(Installation installation)
+        {
+            if (installation == null) return;
+            //wartości serializowane za pomocą konwertera .json
+            Id = installation.Id;
+            LocationString = JsonConvert.SerializeObject(installation.Location);
+            AddressString = JsonConvert.SerializeObject(installation.Address);
+            Elevation = installation.Elevation;
+            IsAirlyInstallation = installation.IsAirlyInstallation;
+        }
+
+        [PrimaryKey]
+        public string Id { get; set; }
+        public string LocationString { get; set; }
+        public string AddressString { get; set; }
+        public double Elevation { get; set; }
+        public bool IsAirlyInstallation { get; set; }
+    }
+}
